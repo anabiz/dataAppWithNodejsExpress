a31d5f84b95323e3c7d62c8798da28c271370cf2
Iwuji Anthony <anabizconcept9@gmail.com>
2020-07-19 14:00:19 +0100

    Update dbservice.js

--- server/dbservice.js
+++ server/dbservice.js
@@ -10,7 +10,6 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT
 });
 //console.log(connection);
-
 connection.connect((err)=>{
      if(err){
          console.log(err.message);
@@ -25,20 +24,84 @@ class DbService {
     async getAllData(){
         try{
             const response = await new Promise((resolve, reject)=>{
-                const query = "SELECT * FROM names;";
-
+                const query = "SELECT * FROM name;";
+                
                 connection.query(query, (err, results)=>{
                     if(err) reject(new Error(err.message));
                     resolve(results);
                 })
             });
-            console.log(response);
+           console.log(response[0].Name);
             return response;
 
         }catch(error){
             console.log(error);
         }
     }
+
+    async insertNewName(name) {
+        try{
+            const dateAdded = new Date();
+            const insertid = await new Promise((resolve, reject)=> {
+                const query ="INSERT INTO name (name, date) VALUE (?,?);";
+
+                connection.query(query, [name, dateAdded],(err, result)=> {
+                    if(err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+           //console.log(insertid);
+            return {
+                id: insertid,
+                name: name,
+                date: dateAdded
+            };
+
+        }catch(error){
+            console.log(error);
+        }
+    }
+    /*
+    insertNewName(name) {
+        try{
+           const dateAdded = new Date();
+           const query ="INSERT INTO name (name, date) VALUE (?,?);";
+
+            connection.query(query, [name, dateAdded],(err, result)=> {
+           // console.log(err);
+            console.log(result);
+            return result.insertId;
+            })
+
+        }catch{
+            throw err;
+        }    
+        
+     // return result;
+
+    }
+    */
+
+    async deleteRowById(id){
+        id = parseInt(id, 10);
+        try{
+            const response = await new Promise((resolve, reject)=> {
+                const query ="DELETE FROM name WHERE id = ?";
+    
+                connection.query(query, [id],(err, result)=> {
+                    if(err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+            console.log(response);
+            return response === 1 ? true : false;
+
+        }catch(error){
+            console.log(error);
+            return false;
+        }
+    }
+    
 }
 
 module.exports = DbService;
